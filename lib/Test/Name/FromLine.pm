ef7bf0adb5166b22c62d2f06174d0bef83b4b8d2
cho45 <cho45@lowreal.net>
2011-06-13 15:08:30 +0900

    init

---
+++ lib/Test/Name/FromLine.pm
@@ -0,0 +1,38 @@
+package Test::Name::FromLine;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Test::Name::FromLine - 
+
+=head1 SYNOPSIS
+
+  use Test::Name::FromLine;
+
+
+=head1 DESCRIPTION
+
+Test::Name::FromLine is 
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
