ef48574a786cac4cfa9f679ef23a87580fda2c14
cho45 <cho45@lowreal.net>
2012-10-23 18:49:01 +0900

    Fix skipping test when $Test::Builder::Level is invalid

--- lib/Test/Name/FromLine.pm
+++ lib/Test/Name/FromLine.pm
@@ -18,13 +18,16 @@ my $ORIGINAL_ok = \&Test::Builder::ok;
 *Test::Builder::ok = sub {
 	$_[2] ||= do {
 		my ($package, $filename, $line) = caller($Test::Builder::Level);
-		$filename or return;
-		$filename = File::Spec->catfile($BASE_DIR, $filename);
-		my $file = $filecache{$filename} ||= [ read_file($filename) ];
-		my $lnum = $line;
-		$line = $file->[$lnum-1];
-		$line =~ s{^\s+|\s+$}{}g;
-		"L$lnum: $line";
+		if ($filename) {
+			$filename = File::Spec->catfile($BASE_DIR, $filename);
+			my $file = $filecache{$filename} ||= [ read_file($filename) ];
+			my $lnum = $line;
+			$line = $file->[$lnum-1];
+			$line =~ s{^\s+|\s+$}{}g;
+			"L$lnum: $line";
+		} else {
+			""; # invalid $Test::Builder::Level
+		}
 	};
 	goto &$ORIGINAL_ok;
 };
